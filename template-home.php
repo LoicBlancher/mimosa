39419aee2fc5f326653b0f021ca081e2f104a758
Miguel Soler <msoler735@gmail.com>
2018-07-24 12:05:25 -0500

    Add template home

---
+++ template-home.php
@@ -0,0 +1,12 @@
+<?php /* Template Name: Home Page Template */ get_header(); ?>
+
+  <main role="main">
+    <!-- section -->
+    <section>
+      <?php the_content(); ?>
+    </section>
+    <!-- /section -->
+  </main>
+
+<?php get_footer();?>
+

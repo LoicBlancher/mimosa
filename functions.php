046ca3581cca12e237b477114cfe86179d7e0a0e
Miguel Soler <msoler735@gmail.com>
2018-07-24 15:08:04 -0500

    Add customize Js for proyect

--- functions.php
+++ functions.php
@@ -118,14 +118,17 @@ function html5blank_header_scripts()
 {
     if ($GLOBALS['pagenow'] != 'wp-login.php' && !is_admin()) {
 
-    	wp_register_script('conditionizr', get_template_directory_uri() . '/js/lib/conditionizr-4.3.0.min.js', array(), '4.3.0'); // Conditionizr
+    	wp_register_script('conditionizr', get_template_directory_uri() . '/js/lib/conditionizr-4.3.0.min.js', array(), '4.3.0',true); // Conditionizr
         wp_enqueue_script('conditionizr'); // Enqueue it!
 
-        wp_register_script('modernizr', get_template_directory_uri() . '/js/lib/modernizr-2.7.1.min.js', array(), '2.7.1'); // Modernizr
+        wp_register_script('modernizr', get_template_directory_uri() . '/js/lib/modernizr-2.7.1.min.js', array(), '2.7.1',true); // Modernizr
         wp_enqueue_script('modernizr'); // Enqueue it!
 
-        wp_register_script('html5blankscripts', get_template_directory_uri() . '/js/scripts.js', array('jquery'), '1.0.0'); // Custom scripts
+        wp_register_script('html5blankscripts', get_template_directory_uri() . '/js/scripts.js', array('jquery'), '1.0.0',true); // Custom scripts
         wp_enqueue_script('html5blankscripts'); // Enqueue it!
+
+        wp_register_script('mimosascript', get_template_directory_uri() . '/js/app.js', array('jquery'), '1.0.0',true); 
+        wp_enqueue_script('mimosascript'); 
     }
 }
 
@@ -146,6 +149,10 @@ function html5blank_styles()
 
     wp_register_style('html5blank', get_template_directory_uri() . '/style.css', array(), '1.0', 'all');
     wp_enqueue_style('html5blank'); // Enqueue it!
+
+    wp_register_style('mimosa', get_template_directory_uri() . '/css/miguel.css', array(), '1.0', 'all');
+    wp_enqueue_style('mimosa'); // Enqueue it!
+    
     wp_enqueue_style('bootstrap',get_template_directory_uri().'/css/bootstrap-grid.min.css'); 
 }
 

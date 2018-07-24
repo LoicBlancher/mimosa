efb75143d58c0a234ccdce0f140b07f08759f941
Miguel Soler <msoler735@gmail.com>
2018-07-24 12:32:37 -0500

    Include Bootstrap for proyect

--- functions.php
+++ functions.php
@@ -146,6 +146,7 @@ function html5blank_styles()
 
     wp_register_style('html5blank', get_template_directory_uri() . '/style.css', array(), '1.0', 'all');
     wp_enqueue_style('html5blank'); // Enqueue it!
+    wp_enqueue_style('bootstrap',get_template_directory_uri().'/css/bootstrap-grid.min.css'); 
 }
 
 // Register HTML5 Blank Navigation

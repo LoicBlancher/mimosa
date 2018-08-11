e1d18940eacbe7c0cc9cd0ad3211fd550195cf9c
msoler18 <msoler735@gmail.com>
2018-08-10 21:22:41 -0500

    Add template page

---
+++ template-thankyou.php
@@ -0,0 +1,9 @@
+<?php /* Template Name: Thank You */ wp_head(); ?>
+<main role="main">
+  <!-- section -->
+  <div class="main">
+    <div class="ms-logTyPage">
+      <img src="<?php echo get_template_directory_uri(); ?>/img/logo-mimosa-test-1.png" alt="" class="ms-logo-ty-pg">
+    </div>
+    <?php the_content(); ?>
+  </div>

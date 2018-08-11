55846287f94fc5609f9dfb0df5dd83b6f3518a78
msoler18 <msoler735@gmail.com>
2018-08-11 00:29:10 -0500

    Add template contact

---
+++ template-contact.php
@@ -0,0 +1,10 @@
+<?php /* Template Name: Contact */ get_header(); ?>
+<main role="main">
+  <!-- section -->
+  <div class="main">
+    <?php the_content(); ?>
+  </div>
+  
+  <!-- /section -->
+</main>
+<?php get_footer('home');?>

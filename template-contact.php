773dfd4d09c5a370ae96283918d2181416232671
msoler18 <msoler735@gmail.com>
2018-08-12 15:09:17 -0500

    Add fixed left and scroll rigth

--- template-contact.php
+++ template-contact.php
@@ -1,9 +1,8 @@
 <?php /* Template Name: Contact */ get_header(); ?>
-<main role="main">
-  <!-- section -->
-  <div class="main">
-    <?php the_content(); ?>
-  </div>
-  <!-- /section -->
-</main>
+    <div class="ms-left-section-fixed">
+      <img src="<?php echo get_template_directory_uri(); ?>/img/contact_map_mimosa.png" alt="" class="ms-contact-map">
+    </div>
+    <div class="ms-right-section-scroll">
+      <?php the_content(); ?>
+    </div>
 <?php get_footer('home');?>

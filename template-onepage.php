f3f4df4ac07ec5b3024ca00872ff71d47ac8ab7c
Miguel Soler <msoler735@gmail.com>
2018-08-29 16:54:47 -0500

    Add first section

---
+++ template-onepage.php
@@ -0,0 +1,13 @@
+<?php /* Template Name: One Page Scroll */ get_header(); ?>
+
+  <main role="main">
+    <!-- section -->
+    <div class="main">
+      <?php the_content(); ?>
+    </div>
+    
+    <!-- /section -->
+  </main>
+</div>
+<?php get_footer('home');?>
+

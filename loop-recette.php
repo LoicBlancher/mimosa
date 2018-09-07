b3d1ac0c48674bfd61aa2c8bb161e48192c3e8a7
Miguel Soler <msoler735@gmail.com>
2018-09-07 18:04:26 -0500

    Finish first styles page notre concept

--- loop-recette.php
+++ loop-recette.php
@@ -3,16 +3,18 @@
         <?php  $loop = new WP_Query( array( 'post_type' => 'recette'));
          if ( $loop->have_posts() ) :
             while ( $loop->have_posts() ) : $loop->the_post(); ?>
-                 <div id="ms-ingredients-card">
-                     <?php 
-                     $image = get_field('image_ingredient');
-                     if( !empty($image) ): ?>
-                      <img src="<?php echo $image['url']; ?>" id="ms-ingredients-image" alt="<?php echo $image['alt']; ?>" />
-                     <?php endif; ?>
-                     <div id="ms-footer-ingredients-card">
-                       <span id="ms-text-footer"><?php the_title()?></span>
-                     </div> 
-                 </div>  
+                <div class="wrapper-ingredients">
+                    <div id="ms-ingredients-card">
+                        <?php 
+                        $image = get_field('image_ingredient');
+                        if( !empty($image) ): ?>
+                         <img src="<?php echo $image['url']; ?>" id="ms-ingredients-image" alt="<?php echo $image['alt']; ?>" />
+                        <?php endif; ?>
+                        <div id="ms-footer-ingredients-card">
+                          <span id="ms-text-footer"><?php the_title()?></span>
+                        </div> 
+                    </div>    
+                </div>
             <?php endwhile;
          endif;?>   
     </div>

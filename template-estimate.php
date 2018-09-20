2b4c95880ac2cf336b1b87de4a0c6c03eda7e603
Miguel Soler <msoler735@gmail.com>
2018-09-20 13:08:14 -0500

    Delete devis template

--- template-estimate.php
+++
@@ -1,9 +0,0 @@
-<?php /* Template Name: Estimate Page */ wp_head(); ?>
-
-<div class="ms-left-section-fixed">
-  <img src="<?php echo get_template_directory_uri(); ?>/img/contact_map_mimosa.png" alt="" class="test-image">
-</div>
-<div class="ms-right-section-scroll">
-  <?php the_content(); ?>
-</div>
-<?php get_footer('home');?>

9d91566c9c8c287651ec7b57363474eb3884606e
msoler18 <msoler735@gmail.com>
2018-10-28 16:31:26 -0500

    Add responsive for clients carousel

--- js/scripts.js
+++ js/scripts.js
@@ -37,7 +37,51 @@ jQuery(function ($) {
         slidesToShow: 3,
         slidesToScroll: 1,
         autoplay: true,
-        autoplaySpeed: 2000
+        autoplaySpeed: 2000,
+        responsive: [
+              {
+                  breakpoint: 1400,
+                  settings: {
+                      slidesToShow: 3,
+                      centerPadding: '400px'
+                  }
+              },
+              {
+                  breakpoint: 1200,
+                  settings: {
+                      slidesToShow: 3,
+                      centerPadding: '300px'
+                  }
+              },
+              {
+                  breakpoint: 992,
+                  settings: {
+                      slidesToShow: 3,
+                      centerPadding: '180px'
+                  }
+              },
+              {
+                  breakpoint: 768,
+                  settings: {
+                      slidesToShow: 2,
+                      centerPadding: '80px'
+                  }
+              },
+              {
+                  breakpoint: 480,
+                  settings: {
+                      slidesToShow: 1,
+                      centerPadding: '25px'
+                  }
+              },
+              {
+                  breakpoint: 360,
+                  settings: {
+                      slidesToShow: 1,
+                      centerPadding: '15px'
+                  }
+              }
+          ]
       });
 
       $('#ms-scroll-ingredients').slick({

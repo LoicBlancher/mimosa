bad50369cc0fa261e5c6e2cbc3ee00cd21e9e509
msoler18 <msoler735@gmail.com>
2018-12-24 12:09:36 -0500

    Add animations for demo culinaire section home

--- js/app.js
+++ js/app.js
@@ -1,20 +1,104 @@
 jQuery(function ($) {
  $(document).ready(function () {
       /* Animations One page template */
-      function animateHome(id){
-         if(id==1){
-           TweenLite.to(".ms-logo-mimosa-main", 2, {scale:1, rotation:20 }); 
-         }
-         else if(id == 2){
-         }
-      }      
+      $(".ms-logo-mimosa-main").on("mouseenter", function() {
+        var duration = 1;
+        TweenMax.to(this, duration / 4, {y:-50, ease:Power2.easeOut});
+        TweenMax.to(this, duration / 2, {y:0, ease:Bounce.easeOut, delay:duration / 4});
+      });   
 
-      function menuTransform(id){
-        if(id==2){
-          let button = $('#socialHeader a.yellowBtn');
-          console.log(button);
-        }
-      }
+      (function animateDemoHome(){
+        egg1 = document.getElementById("ms-container-egg1-home");
+        TweenMax.fromTo(egg1, 0.1, {
+              x: -1,
+            }, {
+              x: 1,
+              repeat: -1,
+              yoyo: true,
+              ease: SlowMo.easeInOut
+            });
+        egg2 = document.getElementById("ms-container-egg2-home");
+        TweenMax.fromTo(egg2, 2, {
+              x: -1,
+            }, {
+              x: 1,
+              repeat: -1,
+              yoyo: true,
+              ease: Quad.easeInOut
+            });
+        egg2 = document.getElementById("ms-container-egg2-home");
+        TweenMax.fromTo(egg2, 0.2, {
+              x: -1,
+            }, {
+              x: 1,
+              repeat: -1,
+              yoyo: true,
+              ease: Quad.easeInOut
+            });
+        bred = document.getElementById("ms-container-bred-home");
+        let radiusBred = 20;
+        TweenLite.set(bred, { 
+          x: -radiusBred, 
+          y: -radiusBred
+        });
+
+        TweenMax.to(bred, 11, {
+          y: radiusBred,
+          ease: Sine.easeInOut,
+          repeat: -1,
+          yoyo: true
+        });
+
+        TweenMax.to(bred, 11, {
+          x: radiusBred,
+          ease: Sine.easeInOut,
+          repeat: -1,
+          yoyo: true
+        }).progress(0.5);
+        spinach = document.getElementById("ms-container-spinach-home");
+        let radiusSpinach = 10;
+        TweenLite.set(spinach, { 
+          x: -radiusSpinach, 
+          y: -radiusSpinach
+        });
+
+        TweenMax.to(spinach, 6, {
+          y: radiusSpinach,
+          ease: Sine.easeInOut,
+          repeat: -1,
+          yoyo: true
+        });
+
+        TweenMax.to(spinach, 6, {
+          x: radiusSpinach,
+          ease: Sine.easeInOut,
+          repeat: -1,
+          yoyo: true
+        }).progress(0.5);
+        
+        /*let demoBackground = $("[background-image=url("'http://mimosa.graditest.com/wp-content/uploads/2018/10/Fondo-2.png'")]");
+        let radiusBack = 20;
+        TweenLite.set(demoBackground, { 
+          x: -radiusBack, 
+          y: -radiusBack
+        });
+
+        TweenMax.to(demoBackground, 6, {
+          y: radiusBack,
+          ease: Sine.easeInOut,
+          repeat: -1,
+          yoyo: true
+        });
+
+        TweenMax.to(demoBackground, 6, {
+          x: radiusBack,
+          ease: Sine.easeInOut,
+          repeat: -1,
+          yoyo: true
+        }).progress(0.5);*/
+      })();
+          
+
 
        /* function showbubble(id){
                       let paginator = document.querySelector(".onepage-pagination li a[href='#"+ id + "']");
@@ -60,10 +144,10 @@ jQuery(function ($) {
                 pagination: true,                
                 updateURL: false,                
                 beforeMove: function(index) {
-                    menuTransform(index);
                 },  
                 afterMove: function(index) {
                     animateHome(index);
+                    animateDemoHome(index);
                 },   
                 loop: false,                     
                 keyboard: true,                  

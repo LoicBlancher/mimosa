7d50d19e60673319caa63124b38278d827b326cf
msoler18 <msoler735@gmail.com>
2019-01-16 15:25:25 -0500

    Migrate js code for quisommes

--- js/animate-qui-sommes.js
+++ js/animate-qui-sommes.js
@@ -1,13 +1,58 @@
 jQuery(function ($) {
  $(document).ready(function () {
             
+ 	(function paginate_carousel(){
+ 	   let button1 = document.getElementById('ms-imgc-1');
+ 	   let button2 = document.getElementById('ms-imgc-2');
+ 	   let button3 = document.getElementById('ms-imgc-3');
+ 	   let image1 = document.getElementById('ms-content-img1');
+ 	   let image2 = document.getElementById('ms-content-img2');
+ 	   let image3 = document.getElementById('ms-content-img3');
+ 	   button1.addEventListener("click",function(){
+ 	       image1.style.display = "block";
+ 	       image2.style.display = "none";
+ 	       image3.style.display = "none"; 
+ 	   });
+ 	   button2.addEventListener("click",function(){
+ 	       image2.style.display = "block";
+ 	       image1.style.display = "none";
+ 	       image3.style.display = "none"; 
+ 	   });
+ 	   button3.addEventListener("click",function(){
+ 	       image3.style.display = "block";
+ 	       image1.style.display = "none";
+ 	       image2.style.display = "none";  
+ 	   });
+
+ 	})();
 
 
 
-
-
-
-                 
+ 	(function eyelashContent(){
+ 	  let btnEyelash1 = document.getElementById('first-eyelash');
+ 	  let btnEyelash2 = document.getElementById('second-eyelash');
+ 	  let btnEyelash3 = document.getElementById('third-eyelash');
+ 	  let content1 = document.getElementById('ms-eyelash-first-qui-sommes');
+ 	  let content2 = document.getElementById('ms-eyelash-second-qui-sommes');
+ 	  let content3 = document.getElementById('ms-eyelash-third-qui-sommes');    
+ 	  btnEyelash1.addEventListener("click",function(){
+ 	      content1.style.display = "block";
+ 	      content2.style.display = "none";
+ 	      content3.style.display = "none";
+ 	  });
+ 	  btnEyelash2.addEventListener("click",function(){
+ 	      content1.style.display = "none";
+ 	      content2.style.display = "block";
+ 	      content3.style.display = "none";
+ 	  });
+ 	  btnEyelash3.addEventListener("click",function(){
+ 	      content1.style.display = "none";
+ 	      content2.style.display = "none";
+ 	      content3.style.display = "block";
+ 	  });
+ 	})();
+ 	
+                
  	 $(".main").onepage_scroll({
  	    sectionContainer: ".section-scroll",     
  	    easing: "ease",                                                                   
--- js/scripts.js
+++ js/scripts.js
@@ -219,65 +219,16 @@ jQuery(function ($) {
         $('#ms-scroll-ingredients').slick('slickPause');
       });
       
-      /* Ms - Carousel Image Qui Sommes */
-      function paginate_carousel(){
-         let button1 = document.getElementById('ms-imgc-1');
-         let button2 = document.getElementById('ms-imgc-2');
-         let button3 = document.getElementById('ms-imgc-3');
-         let image1 = document.getElementById('ms-content-img1');
-         let image2 = document.getElementById('ms-content-img2');
-         let image3 = document.getElementById('ms-content-img3');
-         button1.addEventListener("click",function(){
-             image1.style.display = "block";
-             image2.style.display = "none";
-             image3.style.display = "none"; 
-         });
-         button2.addEventListener("click",function(){
-             image2.style.display = "block";
-             image1.style.display = "none";
-             image3.style.display = "none"; 
-         });
-         button3.addEventListener("click",function(){
-             image3.style.display = "block";
-             image1.style.display = "none";
-             image2.style.display = "none";  
-         });
 
-      };
 
-      /* Ms - Eyelashes Qui Sommes  */
-      function eyelashContent(){
-        let btnEyelash1 = document.getElementById('first-eyelash');
-        let btnEyelash2 = document.getElementById('second-eyelash');
-        let btnEyelash3 = document.getElementById('third-eyelash');
-        let content1 = document.getElementById('ms-eyelash-first-qui-sommes');
-        let content2 = document.getElementById('ms-eyelash-second-qui-sommes');
-        let content3 = document.getElementById('ms-eyelash-third-qui-sommes');    
-        btnEyelash1.addEventListener("click",function(){
-            content1.style.display = "block";
-            content2.style.display = "none";
-            content3.style.display = "none";
-        });
-        btnEyelash2.addEventListener("click",function(){
-            content1.style.display = "none";
-            content2.style.display = "block";
-            content3.style.display = "none";
-        });
-        btnEyelash3.addEventListener("click",function(){
-            content1.style.display = "none";
-            content2.style.display = "none";
-            content3.style.display = "block";
-        });
-      }
-
-      
+/*      
       window.onload = function() {
         if (window.location.href.indexOf('qui-sommes-nous') > -1) {
           eyelashContent();
           paginate_carousel();
         }
       }
-
+*/
       /* Ms Galerie */
       $('.ms-lightbox-trigger').click(function(e){
         e.preventDefault();

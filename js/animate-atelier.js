584ae7b45160c88c1e02d9e4045f78d7f3aed0f7
msoler18 <msoler735@gmail.com>
2019-01-25 14:15:46 -0500

    Modify animation atelier 01 for largest devices

--- js/animate-atelier.js
+++ js/animate-atelier.js
@@ -16,90 +16,84 @@ jQuery(function ($) {
                wH = $(window).height(),
                wS = $(this).scrollTop();
            if (wS > (hT+hH-wH)){
+
               installMediaQueryWatcher("(min-width: 1800px)", function(matches) { 
                 if (matches) {
-                   let el1 = document.getElementById("ms-elementAt1-ser1");
-                   TweenMax.to(el1, 3, {
-                         xPercent: 470,
-                         rotation:360,
-                         ease: Back.easeOut.config(0.2)
+                   let pot = document.getElementById("ms-atelier-pot1");
+                   TweenLite.fromTo(pot, 0.8, {
+                     x:-1}, {
+                       x:1, 
+                       ease:RoughEase.ease.config({
+                         strength:8, 
+                         points:20, 
+                         template:Linear.easeNone, 
+                         randomize:false}) , 
+                     clearProps:"x"})
+                   let el1 = document.getElementById("ms-elementAt9-ser1");
+                   TweenMax.to(el1, 6, {
+                         y:-135,
+                         x:0,
+                         ease: Elastic.easeOut.config(1, 0.3),
                        });
-                   let el2 = document.getElementById("ms-elementAt7-ser1");
+                   let el2 = document.getElementById("ms-elementAt8-ser1");
                    TweenMax.to(el2, 6, {
-                         xPercent: 300,
-                         rotation:350,
-                         ease: Back.easeOut.config(0.2),
-                         onComplete:animateSidesStar()
-                       });
-                       function animateSidesStar(){
-                        let el3 = document.getElementById("ms-elementAt6-ser1");
-                        let el4 = document.getElementById("ms-elementAt8-ser1");
-                        TweenMax.to(el3, 6, {
-                              yPercent: -90,
-                              ease: SteppedEase.config(9)
-                            });
-                        TweenMax.to(el4, 4, {
-                              yPercent: -100,
-                              ease: SteppedEase.config(9)
-                            });
-                       };
+                         delay:2,
+                         y:-585,
+                         x:240,
+                         ease: Elastic.easeOut.config(1, 0.3),
+                       });     
+                    let el3 = document.getElementById("ms-elementAt7-ser1");
+                    TweenMax.to(el3, 6, {
+                          delay:2,
+                          y:-600,
+                          x:5,
+                          ease: Elastic.easeOut.config(1, 0.3),
+                        });     
+                    let el4 = document.getElementById("ms-elementAt6-ser1");
+                    TweenMax.to(el4, 6, {
+                          delay:2,
+                          y:-580,
+                          x:-230,
+                          ease: Elastic.easeOut.config(1, 0.3),
+                        });
                     let el5 = document.getElementById("ms-elementAt5-ser1");
-                    TweenMax.to(el5, 0.8, {
-                          xPercent: -310,
-                          ease: SteppedEase.config(9)
-                        });
-                    let el6 = document.getElementById("ms-elementAt9-ser1");  
+                    TweenMax.to(el5, 6, {
+                          delay:2,
+                          y:-840,
+                          x:280,
+                          ease: Elastic.easeOut.config(1, 0.3),
+                        }); 
+                    let el6 = document.getElementById("ms-elementAt4-ser1");
                     TweenMax.to(el6, 6, {
-                          xPercent: -470,
-                          ease:Bounce.easeOut
+                          delay:2,
+                          y:-700,
+                          x:110,
+                          ease: Elastic.easeOut.config(1, 0.3),
                         });
-                    let el7 = document.getElementById("ms-atelier-pot1");  
+                    let el7 = document.getElementById("ms-elementAt3-ser1");
                     TweenMax.to(el7, 6, {
-                          xPercent: -145,
-                          ease: Elastic.easeOut.config(1, 0.3)
+                          delay:2,
+                          y:-540,
+                          x:-30,
+                          ease: Elastic.easeOut.config(1, 0.3),
+                        });
+                    let el8 = document.getElementById("ms-elementAt2-ser1");
+                    TweenMax.to(el8, 6, {
+                          delay:2,
+                          y:-720,
+                          x:-140,
+                          ease: Elastic.easeOut.config(1, 0.3),
+                        });
+                    let el9 = document.getElementById("ms-elementAt1-ser1");
+                    TweenMax.to(el9, 6, {
+                          delay:2,
+                          y:-580,
+                          x:-300,
+                          ease: Elastic.easeOut.config(1, 0.3),
                         });          
+                             
                 } else {
-                  let el1 = document.getElementById("ms-elementAt1-ser1");
-                  TweenMax.to(el1, 3, {
-                        xPercent: 260,
-                        rotation:360,
-                        ease: Back.easeOut.config(0.2)
-                      });
-                  let el2 = document.getElementById("ms-elementAt7-ser1");
-                  TweenMax.to(el2, 6, {
-                        xPercent: 210,
-                        rotation:350,
-                        scale:0.8,
-                        ease: Back.easeOut.config(0.2),
-                        onComplete:animateSidesStar()
-                      });
-                      function animateSidesStar(){
-                       let el3 = document.getElementById("ms-elementAt6-ser1");
-                       let el4 = document.getElementById("ms-elementAt8-ser1");
-                       TweenMax.to(el3, 6, {
-                             yPercent: -80,
-                             ease: SteppedEase.config(9)
-                           });
-                       TweenMax.to(el4, 4, {
-                             yPercent: -90,
-                             ease: SteppedEase.config(9)
-                           });
-                      };
-                   let el5 = document.getElementById("ms-elementAt5-ser1");
-                   TweenMax.to(el5, 0.8, {
-                         xPercent: -180,
-                         ease: SteppedEase.config(9)
-                       });
-                   let el6 = document.getElementById("ms-elementAt9-ser1");  
-                   TweenMax.to(el6, 6, {
-                         xPercent: -290,
-                         ease:Bounce.easeOut
-                       });
-                   let el7 = document.getElementById("ms-atelier-pot1");  
-                   TweenMax.to(el7, 6, {
-                         xPercent: -75,
-                         ease: SteppedEase.config(9)
-                       });          
+                 
                 }
               }); 
            }

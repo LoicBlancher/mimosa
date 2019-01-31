ccf8c61c41b11ed9e8dc413aed93d3ed95b7550c
msoler18 <msoler735@gmail.com>
2019-01-31 12:04:25 -0500

    Add new map for contact

--- template-contact.php
+++ template-contact.php
@@ -1,7 +1,7 @@
 <?php /* Template Name: Contact Page */ get_header(); ?>
 
 <div class="ms-left-section-fixed">
-  <img src="<?php echo get_template_directory_uri(); ?>/img/contact_map_mimosa.png" alt="" class="test-image">
+  <img src="<?php echo get_template_directory_uri(); ?>/img/map-mimosa-contact.jpg" alt="" class="test-image">
 </div>
 <div class="ms-right-section-scroll">
   <?php the_content(); ?>

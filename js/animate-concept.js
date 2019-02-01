117f62f740ec018de7d0ef1588258acebf426b05
msoler18 <msoler735@gmail.com>
2019-02-01 18:48:06 -0500

    Add animations 03 notre concepts

--- js/animate-concept.js
+++ js/animate-concept.js
@@ -26,6 +26,97 @@ jQuery(function ($) {
 
 
 
+ 		function animation03Concept(id){
+      if(id==3){
+        let $branch2 = $("#ms-part2-force-notrec");
+        let $branch3 = $("#ms-part3-force-notrec");
+        var $branch4 = $("#ms-part4-force-notrec");
+        let $branch5 = $("#ms-part5-force-notrec");
+        let $branch6 = $("#ms-part6-force-notrec");
+        let $branch7 = $("#ms-part7-force-notrec");
+        let $branch8 = $("#ms-part8-force-notrec");
+        let $branch9 = $("#ms-part9-force-notrec");
+        let $branch10 = $("#ms-part10-force-notrec");
+        let $branch11 = $("#ms-part11-force-notrec");
+        let $branch12 = $("#ms-part12-force-notrec");
+        let $branch13 = $("#ms-part13-force-notrec");       
+        TweenMax.fromTo($branch2, 0.1, {
+              x: -1,
+            }, {
+              x: 1,
+              repeat: -1,
+              yoyo: true,
+              ease: SlowMo.easeInOut
+            });
+        TweenMax.fromTo($branch6, 2, {
+              x: -1,
+            }, {
+              x: 1,
+              repeat: -1,
+              yoyo: true,
+              ease: Quad.easeInOut
+            });
+        TweenMax.fromTo($branch8, 0.2, {
+              x: -1,
+            }, {
+              x: 1,
+              repeat: -1,
+              yoyo: true,
+              ease: Quad.easeInOut
+            });
+        let radiusOnion = 20;
+        TweenLite.set($branch12, { 
+          x: -radiusOnion, 
+          y: -radiusOnion
+        });
+
+        TweenMax.to($branch12, 11, {
+          y: radiusOnion,
+          ease: Sine.easeInOut,
+          repeat: -1,
+          yoyo: true
+        });
+
+        TweenMax.to($branch12, 11, {
+          x: radiusOnion,
+          ease: Sine.easeInOut,
+          repeat: -1,
+          yoyo: true
+        }).progress(0.5);
+
+        let radiusBranch11 = 10;
+        TweenLite.set($branch11, { 
+          x: -radiusBranch11, 
+          y: -radiusBranch11
+        });
+
+        TweenMax.to($branch11, 6, {
+          y: radiusBranch11,
+          ease: Sine.easeInOut,
+          repeat: -1,
+          yoyo: true
+        });
+
+        TweenMax.to($branch11, 6, {
+          x: radiusBranch11,
+          ease: Sine.easeInOut,
+          repeat: -1,
+          yoyo: true
+        }).progress(0.5);
+
+        let pathBranch1 = [{x:"900",y:"100"},
+            {x:"100",y:"600"},
+            {x:"20",y:"0"}
+            ];
+        TweenMax.to($branch4,3,{bezier:{curviness:8,values:pathBranch1}},"");
+        let pathBranch6 = [{x:"200",y:"100"},
+            {x:"300",y:"500"},
+            {x:"20",y:"0"}
+            ];
+        TweenMax.to($branch6,3,{bezier:{curviness:8,values:pathBranch6}},"");    
+      };
+    };
+
                  
  	 $(".main").onepage_scroll({
  	    sectionContainer: ".section-scroll",     
@@ -36,6 +127,7 @@ jQuery(function ($) {
  	    beforeMove: function(index) {
  	      header_section_white_concept(index);
  	      header_section_yellow_concept(index);
+        animation03Concept(index);
  	    },  
  	    afterMove: function(index) {
  	    },   

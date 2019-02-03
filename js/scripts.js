e225016b7fd8412556b9a23df52a9365db1e4ed9
msoler18 <msoler735@gmail.com>
2019-02-02 21:21:27 -0500

    Add circle back to top

--- js/scripts.js
+++ js/scripts.js
@@ -299,5 +299,20 @@ jQuery(function ($) {
         });
       })();
       */
+    
+    /* Return to top Pages */
+    // ===== Scroll to Top ==== 
+    $(window).scroll(function() {
+        if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
+            $('#ms-return-to-top').fadeIn(200);    // Fade in the arrow
+        } else {
+            $('#ms-return-to-top').fadeOut(200);   // Else fade out the arrow
+        }
+    });
+    $('#ms-return-to-top').click(function() {      // When arrow is clicked
+        $('body,html').animate({
+            scrollTop : 0                       // Scroll to top of body
+        }, 500);
+    });  
  }); 
 });
--- template-pages.php
+++ template-pages.php
@@ -3,7 +3,7 @@
   <main role="main">
     <!-- section -->
       <?php the_content(); ?>
-    
+      <a href="javascript:" id="ms-return-to-top"><i class="fas fa-chevron-up"></i></a>
     <!-- /section -->
   </main>
 <?php get_footer();?>
